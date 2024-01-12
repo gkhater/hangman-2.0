cc7ddaeed58589bc84291c390ad5545fa8fd7ed5
georges khater <Georges@khater.co>
2024-01-12 14:28:46 +0200

    created decision tree file to judge the difficulty of a word

---
+++ Decision_Tree.py
@@ -0,0 +1,13 @@
+"""
+Decision Tree Implementation
+
+This script is my own implementation of a Decision Tree. It's created to enhance my understanding of how Decision Trees work. 
+
+Credit: 
+    Inspired by "Normalized Nerd" for foundational understanding.
+    This implementation is an acknowledgement of the insights gained from their material, aiming to apply and solidify my knowledge of Decision Trees in a practical manner.
+
+
+Note: This is an educational implementation and may not reflect the performance optimizations of professional machine learning libraries.
+"""
+
